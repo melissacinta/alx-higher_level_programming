023ecbeaf70ba96bd606c1afa24fc992eb12057b
Ngozi Aguluka <melissacinta@gmail.com>
2023-10-03 22:08:24 +0000

    added function to print comma separated numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for num in range(0,100):
+for num in range(00,100):
     if num != 99:
-        print(f"{num}, ", end="")
+        print("{:02}, ".format(num), end="")
     else:
-        print(f"{num}")
+        print("{}".format(num))

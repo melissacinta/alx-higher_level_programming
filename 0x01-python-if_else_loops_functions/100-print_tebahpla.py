e5aa3a4a7a1fe0657c9b878674155723602a6f1b
Ngozi Aguluka <melissacinta@gmail.com>
2023-10-04 19:58:13 +0000

    fixed alphabet bug

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{}{}".format(chr(i), chr(i - 32)), end="")
+i = 0;
+for ch in range(122, 96, -1):
+    print("{}".format(chr(ch - i)), end="")
+    i = 32 if i == 0 else 0

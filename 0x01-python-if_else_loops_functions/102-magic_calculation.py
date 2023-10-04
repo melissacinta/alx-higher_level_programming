c3918c2e25a552ba5135d1f935e8eb071832e8a0
Ngozi Aguluka <melissacinta@gmail.com>
2023-10-04 20:05:51 +0000

    magic again

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -4,3 +4,6 @@ def magic_calculation(a, b, c):
     """Match bytecode provided by Holberton School."""
     if a < b:
         return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)

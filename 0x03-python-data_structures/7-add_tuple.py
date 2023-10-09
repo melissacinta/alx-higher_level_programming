5251bef8d11c76fe592e8c117e92f6b525ab5b0a
Ngozi Aguluka <melissacinta@gmail.com>
2023-10-09 10:05:40 +0000

    add function to add tuples together

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Function to add two tupples contain integers
+'''
+def add_tuple(tuple_a=(), tuple_b=()):
+    list_a, list_b = list(tuple_a), list(tuple_b)
+    new_list = []
+    a, b = 0, 0
+    for i in range(0,2):
+        if i < len(list_a):
+            a = list_a[i]
+        if i < len(list_b):
+            b = list_b[i]
+        new_list.append(a + b)
+    return tuple(new_list)
+

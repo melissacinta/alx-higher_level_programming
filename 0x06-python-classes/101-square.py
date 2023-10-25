f3db948c0af9aac248656616f46933f7e629fa4f
Ngozi Aguluka <melissacinta@gmail.com>
2023-10-25 18:13:48 +0000

    possible fix of pycodestyle error in 101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -53,7 +53,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        """ Define the position of a square"""
+        """Define the position of a square"""
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or

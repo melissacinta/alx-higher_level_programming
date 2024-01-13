d90aadd0623e5a282277b65b8fc99287801f6808
Aguluka Ngozi <melissacinta@gmail.com>
2024-01-13 15:40:06 +0100

    fixed identation error

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,7 +3,7 @@
 
 
 def read_file(filename=""):
-  """Function to read from file and print to the stdout"""
-  with open(filename, encoding="utf-8") as f:
-    read_data = f.read()
-    print(read_data, end='')
+    """Function to read from file and print to the stdout."""
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
